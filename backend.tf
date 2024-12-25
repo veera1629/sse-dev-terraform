7c8912e3d8e6a59ac344f5ac6caa4c277e4c613f
veerareddy <veerareddy.reddy@infosys.com>
2024-12-25 13:27:56 +0530

    updated all documents

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "terraformsa457"
-    container_name       = "terraform-statefile"
-    key                  = "azureinfra_tfstate_file.tfstate"
-    tenant_id            = "dea74a96-f58b-4c55-964e-a5e15d398a68"
-    subscription_id      = "7b3bc1c3-c733-450a-b841-f455c0ce080e"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "terraform-rg"
+#     storage_account_name = "terraformsa457"
+#     container_name       = "terraform-statefile"
+#     key                  = "azureinfra_tfstate_file.tfstate"
+#     tenant_id            = "dea74a96-f58b-4c55-964e-a5e15d398a68"
+#     subscription_id      = "7b3bc1c3-c733-450a-b841-f455c0ce080e"
+#   }
+# }
--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
-# Create a Resource Group if it doesn’t exist
-resource "azurerm_resource_group" "tfexample" {
-  name     = "veera-rg"
-  location = "East US"
+terraform {
+  required_version = ">= 1.5.7"
+  backend "azurerm" {
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "terraformsa457"
+    container_name       = "terraform-statefile"
+    key                  = "azureinfra_tfstate_file.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "veera-rg-001"
+  location = "uksouth"
 }
